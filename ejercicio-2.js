488763e9e02c6fb4cd79379c8c87fd7e31db0464
unknown <ivanvillan54@gmail.com>
2021-12-29 07:09:51 -0300

    Resolución de los ejercicios

--- ejercicio-2.js
+++ ejercicio-2.js
@@ -1,26 +1,36 @@
-let numA = 12345;
-let arr = Array.from(Array(numA).keys());
-var arrResult = [];
+function ejercicio2(numero) {
+	if (numero > 1) {
+		let numA = numero;
+		let arr = Array.from(Array(numA).keys());
+		var arrResult = [];
 
-for(let i in arr) {
-	// Se quitan todos los valores con decimal
-	let resto = numA % arr[i] === 0;
-	if(resto) {
-		let valorEntero = numA / arr[i];
-		arrResult.push([i, valorEntero]);
-	}	
-}
+		for(let i in arr) {
+			// Se quitan todos los valores con decimal
+			let resto = numA % arr[i] === 0;
+			if(resto) {
+				let valorEntero = numA / arr[i];
+				arrResult.push([i, valorEntero]);
+			}	
+		}
 
-for(i = 0; i < arrResult.length; i++) {
-	// Se quitan los valores repetidos
-	if(arrResult.length > 1 && arrResult[i + 1][0] == arrResult[i][1]) {
-		arrResult.splice(i + 1);
+		for(i = 0; i < arrResult.length; i++) {
+			// Se quitan los valores repetidos
+			if(arrResult.length > 1  && arrResult[i + 1][0] == arrResult[i][1]) {
+				arrResult.splice(i + 1);
+			} else {
+				break;
+			}
+		}
+
+		// Se toman los últimos valores del arreglo
+		let numB = parseInt(arrResult[arrResult.length - 1][0]);
+		let numC = arrResult[arrResult.length - 1][1]
+		let resultado = numB + numC;
+		console.log(`B * C = A (${numB * numC})`);
+		console.log(`La suma de los dos factores es ${resultado}`);
+	} else {
+		console.log('Ingresar un numero mas grande');
 	}
 }
 
-// Se toman los últimos valores del arreglo
-let numB = parseInt(arrResult[arrResult.length - 1][0]);
-let numC = arrResult[arrResult.length - 1][1]
-let resultado = numB + numC;
-console.log(numB * numC);
-console.log(resultado);
+ejercicio2(3);
